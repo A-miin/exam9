55bdb43e283c43a9ccfc76728454699f798b3031
A-miin <zulaykaisaeva@gmail.com>
2021-05-29 13:02:41 +0600

    Update settings, requirements.txt, admin.py

--- ex9/ex9/settings.py
+++ ex9/ex9/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+#     custom_appps
+    'gallery',
+    'api',
+    'accounts',
+
+#     lib apps
 ]
 
 MIDDLEWARE = [
--- ex9/gallery/admin.py
+++ ex9/gallery/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import Photo, Album
 # Register your models here.
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ['id','title']
+    fields = ['id', 'title', 'created_at','author','album','is_private','favorite']
+    readonly_fields = ['id']
+
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author']
+    fields = ['id', 'name', 'description', 'author', 'created_at', 'is_private', 'favorite']
+    readonly_fields = ['id']
+
+
+admin.site.register(Photo, PhotoAdmin)
+admin.site.register(Album, AlbumAdmin)
+
